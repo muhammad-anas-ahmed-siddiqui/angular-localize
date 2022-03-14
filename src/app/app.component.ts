8df5efd1aeed7accdcab3c8ebd5e72ef4a1b1f2c
muhammad-anas-ahmed-siddiqui <muhammadanas60606@gmail.com>
2022-03-14 14:28:07 +0500

    locale en "build-locale"

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  title = 'internationalize-localize';
+  minutes = 0;
+  gender = 'female';
+  fly = true;
+  inc(i: number) {
+    this.minutes = Math.min(5, Math.max(0, this.minutes + i));
+  }
+  male() { this.gender = 'male'; }
+  female() { this.gender = 'female'; }
+  other() { this.gender = 'other'; }
 }
